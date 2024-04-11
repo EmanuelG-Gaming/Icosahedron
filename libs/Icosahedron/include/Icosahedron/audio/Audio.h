37f43b7109a03f1f9445ad680122e7f6e2106225
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2024-04-11 18:24:21 +0300

    Add default parameters to sound play functions

--- libs/Icosahedron/include/Icosahedron/audio/Audio.h
+++ libs/Icosahedron/include/Icosahedron/audio/Audio.h
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "soloud.h"
+#include "soloud_audiosource.h"
 
 #include <Icosahedron/audio/Music.h>
 #include <Icosahedron/audio/Sound.h>
@@ -14,20 +15,35 @@
 
 
 namespace ic {
+    enum SoundVolumeAttenuationModels {
+        INVALID_ATTENUATION_MODEL = SoLoud::AudioSource::NO_ATTENUATION,
+        NO_ATTENUATION_MODEL = SoLoud::AudioSource::NO_ATTENUATION,
+
+        INVERSE_DISTANCE_ATTENUATION = SoLoud::AudioSource::INVERSE_DISTANCE,
+        LINEAR_DISTANCE_ATTENUATION = SoLoud::AudioSource::LINEAR_DISTANCE,
+        EXPONENTIAL_DISTANCE_ATTENUATION = SoLoud::AudioSource::EXPONENTIAL_DISTANCE,
+    };
+
     namespace Audio {
+        
         void init();
         void dispose();
 
-        void play(ic::Music *music);
-        void play(ic::Sound *sound);
-        void play3D(ic::Sound *sound, float x, float y, float z, float vx, float vy, float vz, float volume, bool paused);
-        void play3D(ic::Sound *sound, const ic::Vec3 &pos, const ic::Vec3 &vel, float volume, bool paused);
+
+
+        SoLoud::handle play(ic::Music *music, float volume = -1.0f, float panning = 0.0f, bool paused = false);
+        SoLoud::handle play(ic::Sound *sound, float volume = -1.0f, float panning = 0.0f, bool paused = false);
+        SoLoud::handle play3D(ic::Sound *sound, float x, float y, float z, float vx, float vy, float vz, float volume = 1.0f, bool paused = false);
+        SoLoud::handle play3D(ic::Sound *sound, const ic::Vec3 &pos, const ic::Vec3 &vel, float volume = 1.0f, bool paused = false);
 
         void set_listener_position(float x, float y, float z);
         void set_listener_position(const ic::Vec3 &pos);
 
         void set_listener_velocity(float x, float y, float z);
         void set_listener_velocity(const ic::Vec3 &vel);
+
+        void set_source_attenuation_params(SoLoud::handle &handle, float minDistance, float maxDistance, float attenuationRollofFactor, unsigned int attenuationModel);
+
 
         void update();
 
--- libs/Icosahedron/src/audio/Audio.cpp
+++ libs/Icosahedron/src/audio/Audio.cpp
@@ -10,7 +10,6 @@ void ic::Audio::init() {
 
     // By default, SoLoud initializes with OpenGL's right-handed coordinate system
     handler->init(SoLoud::Soloud::CLIP_ROUNDOFF | SoLoud::Soloud::ENABLE_VISUALIZATION);
-
     initialized = true;
 }
 
@@ -30,19 +29,24 @@ void ic::Audio::update() {
     handler->update3dAudio();
 }
 
-void ic::Audio::play(ic::Music *music) {
-    handler->play(music->get_stream());
+SoLoud::handle ic::Audio::play(ic::Music *music, float volume, float panning, bool paused) {
+    return handler->play(music->get_stream(), volume, panning, paused, 0);
 }
-void ic::Audio::play(ic::Sound *sound) {
-    handler->play(sound->get_stream());
+SoLoud::handle ic::Audio::play(ic::Sound *sound, float volume, float panning, bool paused) {
+    return handler->play(sound->get_stream(), volume, panning, paused, 0);
 }
 
-void ic::Audio::play3D(ic::Sound *sound, float x, float y, float z, float vx, float vy, float vz, float volume, bool paused) {
+SoLoud::handle ic::Audio::play3D(ic::Sound *sound, float x, float y, float z, float vx, float vy, float vz, float volume, bool paused) {
     sound->spatial = true;
-    handler->play3d(sound->get_stream(), x, y, z, vx, vy, vz, volume, paused, 0);
+    return handler->play3d(sound->get_stream(), x, y, z, vx, vy, vz, volume, paused, 0);
 }
-void ic::Audio::play3D(ic::Sound *sound, const ic::Vec3 &pos, const ic::Vec3 &vel, float volume, bool paused) {
-    play3D(sound, pos.x(), pos.y(), pos.z(), vel.x(), vel.y(), vel.z(), volume, paused);
+SoLoud::handle ic::Audio::play3D(ic::Sound *sound, const ic::Vec3 &pos, const ic::Vec3 &vel, float volume, bool paused) {
+    return play3D(sound, pos.x(), pos.y(), pos.z(), vel.x(), vel.y(), vel.z(), volume, paused);
+}
+
+void ic::Audio::set_source_attenuation_params(SoLoud::handle &handle, float minDistance, float maxDistance, float attenuationRollofFactor, unsigned int attenuationModel) {
+    handler->set3dSourceMinMaxDistance(handle, minDistance, maxDistance);
+    handler->set3dSourceAttenuation(handle, attenuationModel, attenuationRollofFactor);
 }
 
 
